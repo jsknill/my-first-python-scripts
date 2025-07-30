d8a8bc4eec074e274e31e5b1d9d9095485b7bc49
Jordan K <wotslayer1@proton.me>
2025-07-29 21:55:01 -0400

    Add files via upload

---
+++ my first python script.py
@@ -0,0 +1,17 @@
+my_string = 'Hello World!'
+
+print(my_string)
+
+num = 31
+print(num)
+
+number_as_string = '42'
+number = int(number_as_string)
+
+print(number)
+name = 'Jordan'
+age = 32
+
+print('My name is', name, 'and I am', age, 'years old.')
+
+print(f'My name is {name} and I am {age} years old.')
